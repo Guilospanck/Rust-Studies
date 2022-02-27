35ebbfcdabd82f558157f446e0b7bd073d58883e
Guilherme Rodrigues <guilherme.rp2010@hotmail.com>
2022-02-27 19:26:27 -0300

    feat: writing to a file

--- file_project/src/main.rs
+++ file_project/src/main.rs
@@ -2,11 +2,17 @@ use std::fs::File;
 use std::io::prelude::*;
 
 fn main() {
-  let mut file = File::open("./src/info.txt").expect("Can't open file!");
+  /* Read from a file */
+  let mut file = File::open("info.txt").expect("Can't open file!");
 
   let mut contents = String::new();
   file.read_to_string(&mut contents)
       .expect("Ops! Can not read the file...");
 
   println!("File contents:\n{}", contents);
+
+  /* Write to a file */
+  let mut write_file = File::create("output.txt").expect("Could not create a file");
+  write_file.write_all(b"Wrote to a file").expect("Could not write to the file. Sorry.");
+
 }
